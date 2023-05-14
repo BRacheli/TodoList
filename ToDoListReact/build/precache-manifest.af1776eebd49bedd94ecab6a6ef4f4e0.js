e9ea1d852de9c8cbe627183b026bc0dbbd9b21c4
BRacheli <113826113+BRacheli@users.noreply.github.com>
2023-05-14 16:34:33 +0300

    run build

---
+++ ToDoListReact/build/precache-manifest.af1776eebd49bedd94ecab6a6ef4f4e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159624b0bbdfb1dc23125c27333c3e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e82cfbd771e536faf6b",
+    "url": "/static/js/2.eed7f1e6.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.eed7f1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565ef747c87721256a0b",
+    "url": "/static/js/main.718b93ae.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.718b93ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed24628c41b89e511d36",
+    "url": "/static/js/runtime-main.76e1faf0.js"
+  }
+]);
