a8c23e5188f0e82cd3e82e7960d80bf13757615d
Bracheli <r0556758858@gmail.com>
2023-05-17 00:30:57 +0300

    good commit

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,41 +1,37 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL="http://localhost:5062"
-
 const apiClient=axios.create({
   baseURL:process.env.REACT_APP_API
 })
-
-axios.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    console.log(error);
-  }
-);
+console.log('process.env.API_URL', process.env.REACT_APP_API)
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`/items`)    
+    const result = await apiClient.get(`/items`)    
     return result.data;
   },
-  
+
   addTask: async(name)=>{
-    console.log('addTask', name)
-    const result = await axios.post(`/items`,{"id":0,"name":name,"isComplete":false});
+    const result = await apiClient.post(`/items`,{name:name,isComplete:false})    
     return result.data;
   },
-  
-  setCompleted: async(id, name, isComplete)=>{
-    console.log('setCompleted', {id, isComplete})
-    const result = await axios.put(`/items/${id}`,{"id":id,"name":name,"isComplete":isComplete})
-    return result.data;
+
+  setCompleted: async (id, isComplete) => {
+    await apiClient.put(`/items/${id}?isComplete=${isComplete}`)
   },
-  
+
   deleteTask:async(id)=>{
-    console.log('deleteTask')
-    const result = await axios.delete(`/items/${id}`)
+    const result = await apiClient.delete(`/items/${id}`)    
     return result.data;
   }
 };
+
+apiClient.interceptors.response.use(function (response) {
+  
+  return response;
+}, function (error) {
+  console.error(error);
+  return Promise.reject(error);
+});
+
+
